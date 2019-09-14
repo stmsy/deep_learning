ca52b87ba3054ec292142989bfc817d51840aae4
Masaya Sato <sato.masaya.dev@gmail.com>
2019-09-14 15:51:33 +0900

    Define the AND, OR and NAND gates as single perceptrons

---
+++ perceptrons.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+def eval_and(x1: int, x2: int,  w1: float = 0.5, w2: float = 0.5,
+             b: float = -0.7) -> int:
+    """Evaluate the AND gate as single perceptron."""
+    if np.dot([x1, x2], [w1, w2]) + b >= 0:
+        return 1
+    else:
+        return 0
+
+
+def eval_or(x1: int, x2: int,  w1: float = 0.5, w2: float = 0.5,
+            b: float = -0.2) -> int:
+    """Evaluate the OR gate as single perceptron."""
+    if np.dot([x1, x2], [w1, w2]) + b >= 0:
+        return 1
+    else:
+        return 0
+
+
+def eval_nand(x1: int, x2: int,  w1: float = -0.5, w2: float = -0.5,
+              b: float = 0.7) -> int:
+    """Evaluate the NAND gate as single perceptron."""
+    if np.dot([x1, x2], [w1, w2]) + b >= 0:
+        return 1
+    else:
+        return 0
