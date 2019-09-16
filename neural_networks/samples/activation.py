02324ff3ca676b7f66b2ca8c4b8a65af1bdc6b9b
Masaya Sato <sato.masaya.dev@gmail.com>
2019-09-16 23:46:37 +0900

    Define the softmax function as activation function

--- neural_networks/samples/activation.py
+++ neural_networks/samples/activation.py
@@ -4,24 +4,30 @@ import numpy as np
 
 
 def identity(x: np.array) -> np.array:
-    """Return the value of identity function."""
+    """Return the values of identity function."""
     return x
 
 
 def step(x: np.array, thres: float = 0.0) -> np.array:
-    """Return the value of stepu function following the threshold provided."""
+    """Return the values of stepu function following the threshold provided."""
     y = x >= thres
     return y.astype(np.int)
 
 
 def sigmoid(x: np.array) -> np.array:
-    """Return the value of sigmoid function."""
+    """Return the values of sigmoid function."""
     return 1 / (1 + np.exp(-x))
 
 
 def relu(x: np.array) -> np.array:
-    """Return the value of ReLU function."""
+    """Return the values of ReLU function."""
     return np.maximum(0, x)
+
+
+def softmax(x: np.array) -> np.array:
+    """Return the values of softmax function."""
+    c = np.max(x)
+    return np.exp(x-c) / np.sum(np.exp(x-c))
 
 
 if __name__ == '__main__':
